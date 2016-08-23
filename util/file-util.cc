b373d7abb4d04d8678bb0d77c34f03fe8dff5901
Hieu Le <trunghieule94@gmail.com>
2016-08-23 00:22:06 -0500

    Add new file utilities

--- util/file-util.cc
+++ util/file-util.cc
@@ -2,14 +2,25 @@
 
 #include "util/file-util.h"
 
+#include <fstream>
+#include <streambuf>
+
 #include "boost/filesystem.hpp"
+#include "third_party/easyloggingpp/src/easylogging++.h"
 #include "util/string_util.h"
+
+INITIALIZE_EASYLOGGINGPP
 
 namespace contest_wizard {
 
 bool IsDirectory(const std::string& pathname) {
   boost::filesystem::path path(pathname);
-  return boost::filesystem::is_directory(pathname);
+  return boost::filesystem::is_directory(path);
+}
+
+bool IsFile(const std::string& pathname) {
+  boost::filesystem::path path(pathname);
+  return boost::filesystem::is_regular_file(path);
 }
 
 Status MakeDirectory(const std::string& pathname) {
@@ -18,7 +29,39 @@ Status MakeDirectory(const std::string& pathname) {
     return Status();
   }
   return Status(error::Code::NOT_FOUND,
-                StrCat({"Cannot create directory: ", pathname}));
+                StrCat("Cannot create directory: ", pathname));
+}
+
+Status WriteTextFile(const std::string& pathname, const std::string& content) {
+  if (!IsFile(pathname)) {
+    return Status(error::Code::NOT_FOUND,
+                  StrCat("Text file not found: ", pathname));
+  }
+  std::ofstream output(pathname);
+  output << content;
+  return Status();
+}
+
+Status ReadTextFile(const std::string& pathname, std::string* result) {
+  if (!IsFile(pathname)) {
+    return Status(error::Code::NOT_FOUND,
+                  StrCat("Text file not found: ", pathname));
+  }
+  if (result == nullptr) {
+    return Status(error::Code::INVALID_ARGUMENT,
+                  "Result has not been initialized.");
+  }
+
+  std::ifstream input(pathname);
+  // Allocates all the memory up front rather than relying on string class'
+  // automatic reallocation.
+  input.seekg(0, std::ios::end);
+  result->reserve(input.tellg());
+  input.seekg(0, std::ios::beg);
+
+  result->assign((std::istreambuf_iterator<char>(input)),
+                 std::istreambuf_iterator<char>());
+  return Status();
 }
 
 }  // namespace contest_wizard
--- util/file-util.h
+++ util/file-util.h
@@ -16,12 +16,17 @@ namespace contest_wizard {
 // Checks if the entity referenced by given pathname exists and is a directory.
 bool IsDirectory(const std::string& pathname);
 
+// Checks if the entity referenced by given pathname exists and is a file.
+bool IsFile(const std::string& pathname);
+
 // Creates a directory referenced by given pathname.
 Status MakeDirectory(const std::string& pathname);
 
-bool WriteTextFile(const std::string& filename, const std::string& content);
+// Writes content to a file referenced by pathname.
+Status WriteTextFile(const std::string& pathname, const std::string& content);
 
-std::string ReadTextFile(const std::string& filename);
+// Reads to result from a file referenced by pathname.
+Status ReadTextFile(const std::string& pathname, std::string* result);
 
 std::unique_ptr<Task> ReadTask(const std::string& filename);
 
